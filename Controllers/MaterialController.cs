a77ca666b81e8b9bdd11a0bdf0677f6ef042325a
outsider4444 <danila_kuzin_2001@mail.ru>
2021-09-20 02:36:22 +0400

    Добавлен Editor (Необходимо привязать к БД)

---
+++ Controllers/MaterialController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SixthWeb.Controllers
+{
+    public class MaterialController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
